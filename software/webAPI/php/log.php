8dbf53424b6b03d7e23457696c16fa48512e2689
moca0511 <jun.moca.1038@gmail.com>
2021-08-17 10:49:33 +0900

    中間発表資料追加、webAPIpost機能追加

---
+++ software/webAPI/php/log.php
@@ -0,0 +1,33 @@
+
+<HTML>
+<HEAD>
+<TITLE>log</TITLE>
+<?php
+    $nowDay = date("Y/m/d");
+    $nowTime = date("H:i:s");
+    $state = $_POST["state"];
+    $ymd = $_POST["ymd"];
+    $time = $_POST["time"];
+    // $sOrA = $_POST["start_arrival"];
+    // echo "post example:";
+    // echo $state;
+    // echo "<br>";
+    if(!$FP = fopen("../log/log.csv","a"))
+        echo "fopen error";
+    else{
+        $arr=array($nowDay,$nowTime,$state,$ymd,$time);
+        print_r(array_values($arr));
+        if(!fputcsv($FP,$arr))
+            echo "<br>書込に失敗しました";
+        else
+            echo "<br>データを書込ました";
+        fclose($FP);
+    }
+?>
+</HEAD>
+<BODY onLoad=setTimeout("location.href='../index.html'",0)>
+<!-- <P>約3秒後にジャンプします(JavaScript版)</P> -->
+</BODY>
+</HTML>
+
+

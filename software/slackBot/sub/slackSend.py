ebc1de3dd656851042ae2c1cc9faf49b09dc104d
石橋尚之 <st031864@m02.kyoto-kcg.ac.jp>
2021-11-18 18:47:07 +0900

    slackのチャンネルにメッセージを読み込むプログラムと書き込むpythonプログラムを追加 slackのtokenが必要

---
+++ software/slackBot/sub/slackSend.py
@@ -0,0 +1,38 @@
+# python3.6
+import json
+import requests
+import sys
+
+
+# Jsonファイル読み込み
+# 引数:設定ファイル
+# 戻り値:ファイルの中身全て
+def getJsonFile(fileName):
+    with open(fileName, 'r') as f:
+        return json.loads(f.read())
+
+
+# slackのチャンネルにメッセージ送信
+# 戻り値:送信結果のjson
+def sendMessage(url, header, chId, text):
+    data = {'channel': chId, 'text': text}
+    return requests.post(url, headers=header, data=data)
+
+
+api_keys = getJsonFile("./config/token.json")  # apiKeysをファイルから読み込み
+
+# slack送信用データを設定
+token = api_keys['token_user']
+url_send = "https://slack.com/api/chat.postMessage"
+head = {"Authorization": "Bearer {}".format(token)}
+channelId = api_keys['channel']
+
+# 送信用テキストデータの設定
+args = sys.argv
+mySendMessage = 'メッセージ送信のテストです。'
+if 2 <= len(args):
+    mySendMessage = args[1]
+
+# メッセージの送信と結果の読み込み
+r = sendMessage(url_send, head, channelId, mySendMessage)
+print("return ", r.json())  # 送信成功と失敗の確認用
